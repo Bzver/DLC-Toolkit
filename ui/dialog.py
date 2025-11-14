9f45000e93e03b7bb20fbb5a3516551e95e47959
Bzver <t.c.pedrovsky@gmail.com>
2025-11-14 13:30:06 +0800

    Remove redundant import

--- ui/dialog.py
+++ ui/dialog.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt, Signal
 from PySide6.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout, QDialog, QLabel, QMessageBox, QSpinBox, QCheckBox
