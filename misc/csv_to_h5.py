cc60fb4d4b06dc2df8eba6ba10ea0d844a051069
Bzver <t.c.pedrovsky@gmail.com>
2025-12-04 13:19:21 +0800

    Remove redundant file and update gitignore

--- misc/csv_to_h5.py
+++
@@ -1,9 +0,0 @@
-# from core.io import csv_op
-
-# if __name__ == "__main__":
-#     csv_file = "CollectedData_bezver"
-#     project_dir = "D:/Project/DLC-Models/NTD/labeled-data/SD-20250605B-cam5_cam5"
-#     if csv_op.csv_to_h5(project_dir=project_dir, multi_animal=True,scorer="bezver",csv_name=csv_file):
-#         print("Success.")
-
-# # Move it to the root folder ( DLC-Toolkit/ ) to use,
